ab57809bc0d0ae45e986813b14e90bfcf6871761
tuananh1208 <trantuananh120803@gmail.com>
2023-04-19 19:32:46 +0700

    fix huffman code

--- restaurant.cpp
+++ restaurant.cpp
@@ -6,19 +6,19 @@ template <class T>
 class HuffNode {
 public:
 	virtual ~HuffNode() {}	// Destructor
-	virtual int frequency() = 0;
+	virtual int freq() = 0;
 	virtual bool isLeaf() = 0;
 };
 
 template <class T>	// Leaf node subclass
 class LeafNode : public HuffNode<T> {
 private:
-	T data;   // Data
-	int freq; // Frequency
+	T Data;   // Data
+	int Freq; // freq
 public:
-	LeafNode(const T& data, int freq) : data(data), freq(freq) {}
-	T getData() { return data; }
-	int frequency() { return freq; }
+	LeafNode(const T& data, int freq) : Data(data), Freq(freq) {}
+	T data() { return Data; }
+	int freq() { return this->Freq; }
 	bool isLeaf() { return true; }
 };
 
@@ -27,14 +27,14 @@ class IntlNode : public HuffNode<T> {
 private:
 	HuffNode<T>* left; // Left child
 	HuffNode<T>* right; // Right child
-	int freq; // Frequency
+	int Freq; // freq
 public:
 	IntlNode(HuffNode<T>* leftChild, HuffNode<T>* rightChild) {
-		freq = leftChild->frequency() + rightChild->frequency();
+		this->Freq = leftChild->freq() + rightChild->freq();
 		this->left = leftChild;
 		this->right = rightChild;
 	}
-	int frequency() { return freq; }
+	int freq() { return this->Freq; }
 	bool isLeaf() { return false; }
 
 	HuffNode<T>* getLeft() { return left; }
@@ -46,23 +46,24 @@ public:
 
 template <class T>
 class HuffTree {
-private:
-	HuffNode<T>* root;
+public: // change to private
+	HuffNode<T>* Root;
 public:
 	HuffTree(T data, int freq) {	// Leaf constructor
-		root = new LeafNode<T>(data, freq);
+		Root = new LeafNode<T>(data, freq);
 	}
 
 	HuffTree(HuffTree<T>* left, HuffTree<T>* right) {		// Internal node constructor
-		root = new IntlNode<T>(left->root, right->root);
+		Root = new IntlNode<T>(left->root(), right->root());
 	}
 
 	~HuffTree() { // Destructor
-		deleteHuffTree(root);
+		deleteHuffTree(this->Root);
 	}	
-	HuffNode<T>* getRoot() { return root; }
-	int frequency() { return root->frequency(); }
-
+	
+	HuffNode<T>* root() { return Root; }
+	int freq() { return Root->freq(); }
+	bool isLeaf() { return Root->isLeaf(); }
 private:
 	void deleteHuffTree(HuffNode<T>* node) {
 		if (node) {
@@ -75,8 +76,16 @@ private:
 
 template <class T>
 struct Compare {
-    bool operator()(HuffTree<T>* a, HuffTree<T>* b) {
-        return a->frequency() > b->frequency();
+    bool operator()(pair<HuffTree<char>*, int> a, pair<HuffTree<char>*, int> b) {
+		if (a.first->freq() == b.first->freq()) {
+			if (a.first->isLeaf() && b.first->isLeaf()) {
+				return static_cast<LeafNode<char>*>(a.first->root())->data() > static_cast<LeafNode<char>*>(b.first->root())->data();
+			} else {
+				return a.second > b.second;
+			}
+		} else {
+			return a.first->freq() > b.first->freq();
+		}
     }
 };
 
@@ -85,22 +94,34 @@ HuffTree<char>* buildHuffTree(string name) {
 	for (char x : name) {
 		freq_map[x]++;
 	}
-
-	priority_queue<HuffTree<char>*, vector<HuffTree<char>*>, Compare<char>> freq_queue;
+	int order = 0;
+	priority_queue<pair<HuffTree<char>*, int>, vector<pair<HuffTree<char>*, int>>, Compare<char>> freq_queue;//, temp_queue;
 	for (auto x : freq_map) {
-		freq_queue.push(new HuffTree<char>(x.first, x.second));
+		freq_queue.push(make_pair(new HuffTree<char>(x.first, x.second), order++));
+		//temp_queue.push(make_pair(new HuffTree<char>(x.first, x.second), order++));
 	}
+
+	// while (temp_queue.size() > 0) {// del
+	// 	cout << temp_queue.top().first->freq() << "-" << static_cast<LeafNode<char>*>(temp_queue.top().first->root())->data() << ";";// del
+	// 	temp_queue.pop();// del
+	// }// del
+	// cout << endl;// del
 
 	while (freq_queue.size() > 1) {
-		HuffTree<char>* left = freq_queue.top();
+		HuffTree<char>* left = freq_queue.top().first;
 		freq_queue.pop();
-		HuffTree<char>* right = freq_queue.top();
+		HuffTree<char>* right = freq_queue.top().first;
 		freq_queue.pop();
 		HuffTree<char>* parent = new HuffTree<char>(left, right);
-		freq_queue.push(parent);
-	}
-
-	return freq_queue.top();
+		freq_queue.push(make_pair(parent, order++));
+		//temp_queue = freq_queue; del
+		// while (temp_queue.size() > 0) {// del
+		// 	cout << temp_queue.top().first->freq() << "-" << static_cast<LeafNode<char>*>(temp_queue.top().first->root())->data() << ";";// del
+		// 	temp_queue.pop();// del
+		// }// del
+		// cout << endl;// del
+	}	
+	return freq_queue.top().first;
 }
 
 void encodeHuffTree(HuffNode<char>* node, string path, unordered_map<char, string>& code_map) {
@@ -109,7 +130,7 @@ void encodeHuffTree(HuffNode<char>* node, string path, unordered_map<char, strin
 	}
 
 	if (node->isLeaf()) {
-		code_map[static_cast<LeafNode<char>*>(node)->getData()] = path;
+		code_map[static_cast<LeafNode<char>*>(node)->data()] = path;
 	} else {
 		encodeHuffTree(static_cast<IntlNode<char>*>(node)->getLeft(), path + "0", code_map);
 		encodeHuffTree(static_cast<IntlNode<char>*>(node)->getRight(), path + "1", code_map);
@@ -120,7 +141,7 @@ string getHuffString(string text) {
 	HuffTree<char>* tree = buildHuffTree(text);
 	unordered_map<char, string> code_map;
 
-	encodeHuffTree(tree->getRoot(), "", code_map);
+	encodeHuffTree(tree->root(), "", code_map);
 
 	string Huff_string;
 	for (char x : text) {
@@ -160,16 +181,57 @@ void reg(string command) {
 		return;
 	} 
 
-	string Huff_string = getHuffString(name);
+	string Huff_string = getHuffString(name); 
+	cout << Huff_string << endl; // del
 	if (Huff_string.size() > 15) {
 		int start = Huff_string.size() - 15;
 		int end = Huff_string.size() - 1;
 		Huff_string = Huff_string.substr(start, end - start + 1);
 	}
+	
 
 	int Huff_dec = convertBinToDec(Huff_string);
+	cout << Huff_dec << endl; // del
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void simulate(string filename)
 {
